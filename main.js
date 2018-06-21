7419ecc7527f482ec195fc4c8ad8ed00a3352c88
Janine Hempy <jhempy@gmail.com>
2018-06-21 11:38:27 -0400

    Refactors to use components

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+Vue.component('task', {
+
+	props: [ 'words', 'index' ],
+
+	template: `<li class="list-group-item" v-on:click="signalDelete(index)">
+				{{ words }}
+				<button type="button" class="close" aria-label="Close">
+  					<span aria-hidden="true" class="text-danger">&times;</span>
+				</button>
+			</li>`,
+
+	methods: {
+		signalDelete(index) {
+			console.log("Emitting a delete event for [" + index + "] in task component");
+			this.$emit('delete', index);
+		}
+	}
+
+});
+
+
 var todo = new Vue({
 
 	el: '#main',
@@ -21,6 +42,7 @@ var todo = new Vue({
 
 		removeTask: function(index) {
 
+			console.log("Here we are, in removeTask, trying to delete [" + index + "]");
 			this.tasks.splice(index, 1);
 
 		}
